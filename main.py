ee16bff5d29cb07987cb6ef464cd71038442030c
RedC0mrade <e2e4e2e4@mail.ru>
2024-07-18 19:30:57 +0200

    Первые шаги

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from starlette.responses import FileResponse, JSONResponse
 
 app = FastAPI()
 
 
 @app.get('/')
 async def root():
-    return {'message': 'Hello World'}
+    return FileResponse('index.html')
+
+
+@app.post('/calculate')
+async def calculate(num1: int, num2: int):
+    return {"result": num1 + num2}
