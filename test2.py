07164809f80a6bec7e232be65351792c6164110e
RedC0mrade <e2e4e2e4@mail.ru>
2024-08-28 20:29:00 +0200

    Первые шаги

--- test2.py
+++ test2.py
@@ -48,19 +48,3 @@ items = [
 ]
 
 ts = TreeStore(items)
-
-# Примеры использования:
-#print(ts.getAll())
-# [{"id":1,"parent":"root"},{"id":2,"parent":1,"type":"test"},{"id":3,"parent":1,"type":"test"},{"id":4,"parent":2,"type":"test"},{"id":5,"parent":2,"type":"test"},{"id":6,"parent":2,"type":"test"},{"id":7,"parent":4,"type":None},{"id":8,"parent":4,"type":None}]
-
-#print(ts.getItem(7))
-# {"id":7,"parent":4,"type":None}
-
-print(ts.getChildren(4))
-# [{"id":7,"parent":4,"type":None},{"id":8,"parent":4,"type":None}]
-
-#print(ts.getChildren(5))
-# []
-
-#print(ts.getAllParents(7))
-# [{"id":4,"parent":2,"type":"test"},{"id":2,"parent":1,"type":"test"},{"id":1,"parent":"root"}]
